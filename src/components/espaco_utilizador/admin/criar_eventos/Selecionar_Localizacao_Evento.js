fd22ecebf57e62dd5a5fe666fa18157cff50118d
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2023-01-22 00:37:32 +0000

    Maps

--- src/components/espaco_utilizador/admin/criar_eventos/Selecionar_Localizacao_Evento.js
+++ src/components/espaco_utilizador/admin/criar_eventos/Selecionar_Localizacao_Evento.js
@@ -13,10 +13,19 @@ export default function Selecionar_Localizacao_Evento(){
             <div class="card-body">
                 {/* How to use HERE Maps - https://developer.here.com/tutorials/getting-here-credentials/ */}
                 <div className="google-map-code">
+                    <iframe src="https://embed.waze.com/iframe?zoom=15&lat=37.100372&lon=-8.268414&ct=livemap"
+                            width="600" height="450" allowFullScreen></iframe>
+
+                    <br/>
+
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15282225.79979123!2d73.7250245393691!3d20.750301298393563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1587818542745!5m2!1sen!2sin"
-                        width="600" height="450" frameBorder="0" allowFullScreen=""
-                        aria-hidden="false" tabIndex="0"></iframe>
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12717.634883626057!2d-8.297905799999999!3d37.166756649999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-PT!2spt!4v1674343609983!5m2!1spt-PT!2spt"
+                        width="600" height="450" allowFullScreen="" loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+                    <br/>
+
+                    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1f8BYUCwFk8Ue0fG7c3cZ8Ia7Yiiczow&ehbc=2E312F" width="640" height="480"></iframe>
 
                 </div>
             </div>
