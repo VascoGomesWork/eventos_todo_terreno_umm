9ba9a6d7d3ddc655dfdf02a02cdff0d02fa9cb45
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2023-01-30 13:18:50 +0000

    Update

--- src/components/espaco_utilizador/cliente/Nav.js
+++ src/components/espaco_utilizador/cliente/Nav.js
@@ -8,9 +8,7 @@ export default function Nav(){
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             {/*-- Navbar Brand--*/}
             <div>
-                <a className="navbar-brand ps-3" href="/"><img
-                    src="../../../../img/umm_logo2.jpg"/>Eventos
-                    Todo-o-Terreno UMM</a>
+                <a className="navbar-brand ps-3" href="/"><img src="../../../../img/umm_logo2.jpg"/> Eventos Todo-o-Terreno UMM</a>
             </div>
             {/*-- Sidebar Toggle--*/}
             <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><span
