187f74ca88363fdb421319bf5bdd6b32d5350969
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2023-02-06 01:36:00 +0000

    Update

--- src/components/index/Nav.js
+++ src/components/index/Nav.js
@@ -7,7 +7,7 @@ export default function Nav(){
         {/* Navigation */},
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top"><img src="../../img/umm_logo2.jpg"/>Eventos Todo o Terreno UMM</a>
+                <a class="navbar-brand" href="#page-top"><img src="../../img/umm_logo2.jpg"/> Eventos Todo-o-Terreno UMM</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto my-2 my-lg-0">
